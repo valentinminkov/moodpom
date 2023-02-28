67343f468a225468aab3efdbc9ebfb45eb74c327
Valentin Minkov <minkowalentin@proton.me>
2023-02-28 22:56:47 +0100

    use only css for the transition. fix the scroll issue with css

--- src/components/BreakContent/BreakContent.jsx
+++ src/components/BreakContent/BreakContent.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import styles from "./BreakContent.module.css";
 
-const BreakContent = () => {
+const BreakContent = ({ className: propClasses }) => {
   return (
-    <div className={styles["break-content"]}>
-      <h1>Banana</h1>
-      <p>
+    <div className={`${styles["break-content"]} ${propClasses}`}>
+      <h1 className={styles.title}>Lorem ipsum dolor sit amet consectetur.</h1>
+      <p className={styles.paragraph}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione placeat
         cupiditate nihil autem ab eius quas dolor ex quo, magnam nemo adipisci
         molestias animi voluptates officia cumque id iusto recusandae!
