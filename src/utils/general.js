2843a31bf72c476c150a0bb0a4a3a3a7de9fbe52
Valentin Minkov <minkowalentin@proton.me>
2023-02-28 22:58:57 +0100

    remove unused utils

--- src/utils/general.js
+++ src/utils/general.js
@@ -4,18 +4,6 @@ const convertToMinutes = (time) => {
   return time * MINUTE;
 };
 
-const addClassWithDelay = (targetContainerClass, classToAdd, delay) => {
-  setTimeout(() => {
-    const breakContainer = document.querySelector(`.${targetContainerClass}`);
-    breakContainer.classList.add(classToAdd);
-  }, delay);
-};
 
-const removeClassWithDelay = (targetContainerClass, classToRemove, delay) => {
-  setTimeout(() => {
-    const breakContainer = document.querySelector(`.${targetContainerClass}`);
-    breakContainer.classList.remove(classToRemove);
-  }, delay);
-};
 
-export { convertToMinutes, addClassWithDelay, removeClassWithDelay };
+export { convertToMinutes };
