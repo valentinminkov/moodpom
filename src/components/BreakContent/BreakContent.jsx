df58dbd49b632e52f5c1d876667d6a954f4ed6d2
Valentin Minkov <minkowalentin@proton.me>
2023-03-01 21:28:54 +0100

    add dummy breathing exercises

--- src/components/BreakContent/BreakContent.jsx
+++ src/components/BreakContent/BreakContent.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import styles from "./BreakContent.module.css";
+import { breathingExercises } from "../../dummyData/data";
 
 const BreakContent = ({ className: propClasses }) => {
   return (
     <div className={`${styles["break-content"]} ${propClasses}`}>
-      <h1 className={styles.title}>Lorem ipsum dolor sit amet consectetur.</h1>
-      <p className={styles.paragraph}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione placeat
-        cupiditate nihil autem ab eius quas dolor ex quo, magnam nemo adipisci
-        molestias animi voluptates officia cumque id iusto recusandae!
-      </p>
+      <h1 className={styles.title}>{breathingExercises[0].name}</h1>
+      <p className={styles.paragraph}>{breathingExercises[0].description}</p>
     </div>
   );
 };
---
+++ src/dummyData/data.js
@@ -0,0 +1,94 @@
+const breathingExercises = [
+  {
+    name: "Box Breathing",
+    description:
+      "Breath in for four seconds, hold your breath for four seconds, breath out for four seconds, and holding your breath for four seconds. This cycle is repeated for a total of four minutes. This is a simple breathing exercise that can help you relax and reduce stress.",
+  },
+  {
+    name: "Alternate Nostril Breathing",
+    description:
+      "Alternate nostril breathing is a yoga breathing technique that involves inhaling and exhaling through alternate nostrils, using hand gestures to alternate the flow of air. This practice is believed to help balance the energy channels in the body and calm the mind.",
+  },
+  {
+    name: "4-7-8 Breathing",
+    description:
+      "4-7-8 breathing is a simple breathing technique that involves inhaling for 4 seconds, holding the breath for 7 seconds, and exhaling for 8 seconds. This technique is believed to help calm the nervous system, reduce stress and anxiety, and improve sleep. To practice 4-7-8 breathing, sit comfortably with a straight spine, inhale through your nose for 4 seconds, hold your breath for 7 seconds, then exhale through your mouth for 8 seconds, making a whooshing sound as you exhale. Repeat this cycle for several rounds, focusing on slow, deep breaths and a relaxed body.",
+  },
+  {
+    name: "Belly Breathing",
+    description:
+      "Belly breathing, also known as diaphragmatic breathing, is a technique that involves breathing deeply into your belly, rather than shallowly into your chest. This practice can help reduce stress and anxiety, increase relaxation, and improve digestion. To practice belly breathing, sit or lie down in a comfortable position and place your hand on your belly. Breathe in slowly through your nose, filling your belly with air and pushing your hand out, then exhale slowly through your mouth, letting your belly fall inward. Repeat for several deep, slow breaths.",
+  },
+  {
+    name: "Buddhist Breathing",
+    description:
+      "Buddhist breathing, also known as mindfulness breathing, is a technique that involves focusing on the sensation of your breath as a way to cultivate mindfulness and awareness. To practice Buddhist breathing, sit comfortably with a straight spine and bring your attention to your breath. Notice the sensation of the air moving in and out of your nose, the rise and fall of your chest or belly, and any other physical sensations associated with your breath. If your mind wanders, gently bring it back to your breath without judgment. Repeat for several minutes.",
+  },
+  {
+    name: "Chest Breathing",
+    description:
+      "Chest breathing, also known as shallow breathing, is a common breathing pattern that involves breathing into your chest rather than your belly. This type of breathing can be associated with stress, anxiety, and poor posture. To practice chest breathing, sit or stand up straight and breathe in deeply through your nose, feeling your chest expand. Exhale through your mouth, letting your chest fall. Repeat for several breaths, focusing on expanding and contracting your chest with each inhale and exhale.",
+  },
+  {
+    name: "Diaphragmatic Breathing",
+    description:
+      "Diaphragmatic breathing, also known as belly breathing, is a technique that involves breathing deeply into your belly, rather than shallowly into your chest. This practice can help reduce stress and anxiety, increase relaxation, and improve digestion. To practice diaphragmatic breathing, sit or lie down in a comfortable position and place your hand on your belly. Breathe in slowly through your nose, feeling your belly rise, then exhale slowly through your mouth, feeling your belly fall. Repeat for several deep, slow breaths.",
+  },
+  {
+    name: "Equal Breathing",
+    description:
+      "Equal breathing is a breathing technique that involves breathing in and out for an equal count of time. This practice can help calm the mind, reduce stress, and improve focus. To practice equal breathing, sit comfortably with a straight spine and breathe in through your nose for a count of four, then exhale through your nose for a count of four. Repeat for several rounds, gradually increasing the count if comfortable.",
+  },
+  {
+    name: "Full Yogic Breathing",
+    description:
+      "Full Yogic Breathing is a yoga breathing technique that involves breathing deeply into your belly, then expanding your ribcage, and finally filling your chest with air. This practice can help increase oxygen intake, improve lung capacity, and reduce stress. To practice Full Yogic Breathing, sit or lie down in a comfortable position and place your hands on your belly. Breathe in deeply through your nose, feeling your belly rise, then expand your ribcage and feel your chest fill with air. Exhale slowly through your nose, feeling your chest and ribcage fall, then your belly. Repeat for several deep, slow breaths.",
+  },
+  {
+    name: "Humming Breathing",
+    description:
+      "Humming breathing, also known as Brahmari pranayama, is a yoga breathing technique that involves making a humming sound while exhaling. This practice can help reduce stress, anxiety, and tension, and promote relaxation. To practice humming breathing, sit comfortably with a straight spine and close your eyes. Breathe in deeply through your nose, then exhale through your nose while making a low-pitched humming sound. Repeat for several rounds, focusing on the sensation of the sound and vibration in your body.",
+  },
+  {
+    name: "Lion's Breath",
+    description:
+      "Lion's breath, also known as Simhasana pranayama, is a yoga breathing technique that involves a forceful exhalation while sticking out your tongue and making a roaring sound. This practice can help release tension, improve circulation, and stimulate the respiratory system. To practice Lion's breath, sit in a comfortable position with a straight spine and open your mouth wide. Breathe in deeply through your nose, then exhale forcefully through your mouth while sticking out your tongue and making a loud 'ha' or 'roar' sound. Repeat for several rounds.",
+  },
+  {
+    name: "Meditative Breathing",
+    description:
+      "Meditative breathing is a simple breathing technique that involves bringing your awareness to your breath as a way to cultivate mindfulness and calmness. This practice can help reduce stress, increase focus, and improve well-being. To practice meditative breathing, sit comfortably with a straight spine and bring your attention to your breath. Notice the sensation of the air moving in and out of your nose or mouth, the rise and fall of your chest or belly, and any other physical sensations associated with your breath. If your mind wanders, gently bring it back to your breath and repeat for several minutes, focusing on the present moment.",
+  },
+  {
+    name: "Nadi Shodhana",
+    description:
+      "Nadi Shodhana, also known as alternate nostril breathing, is a yoga breathing technique that involves breathing through alternate nostrils. This practice can help balance the energy channels in the body, calm the mind, and reduce stress. To practice Nadi Shodhana, sit comfortably with a straight spine and use your right hand to close your right nostril with your thumb. Inhale deeply through your left nostril, then close your left nostril with your ring finger and release your thumb to exhale through your right nostril. Inhale through your right nostril, then close it with your thumb and release your ring finger to exhale through your left nostril. Repeat for several rounds, alternating the nostrils.",
+  },
+  {
+    name: "Ocean Breathing",
+    description:
+      "Ocean breathing, also known as Ujjayi pranayama, is a yoga breathing technique that involves breathing through the back of your throat to create a hissing or ocean-like sound. This practice can help increase relaxation, improve concentration, and promote mindfulness. To practice ocean breathing, sit comfortably with a straight spine and inhale deeply through your nose. As you exhale, constrict the back of your throat to create a hissing sound, as if you're trying to fog up a mirror with your breath. Repeat for several rounds, focusing on the sound and sensation of your breath.",
+  },
+  {
+    name: "Pursed Lip Breathing",
+    description:
+      "Pursed lip breathing is a simple breathing technique that involves inhaling deeply through your nose and exhaling through pursed lips. This practice can help improve lung function, reduce shortness of breath, and promote relaxation. To practice pursed lip breathing, sit comfortably with a straight spine and inhale deeply through your nose. Purse your lips as if you're going to whistle, then exhale slowly and steadily through your lips, feeling a slight resistance. Repeat for several breaths.",
+  },
+  {
+    name: "Sama Vritti",
+    description:
+      "Sama Vritti, also known as equal breathing, is a breathing technique that involves breathing in and out for an equal count of time. This practice can help calm the mind, reduce stress, and improve focus. To practice Sama Vritti, sit comfortably with a straight spine and breathe in through your nose for a count of four, then exhale through your nose for a count of four. Repeat for several rounds, gradually increasing the count if comfortable.",
+  },
+  {
+    name: "Sudarshan Kriya",
+    description:
+      "Sudarshan Kriya is a breathing technique that combines slow, medium, and fast cycles of breath to help balance the body, mind, and emotions. This practice can help reduce stress, improve focus, and promote overall well-being. To practice Sudarshan Kriya, sit comfortably with a straight spine and follow the guided instructions, which typically involve breathing in for a specific count, holding the breath, and exhaling for a specific count. Repeat for several rounds, focusing on the sensation of your breath and the changes in your body.",
+  },
+  {
+    name: "Ujjayi Breathing",
+    description:
+      "Ujjayi breathing is a yoga breathing technique that involves breathing through the back of your throat to create a hissing or ocean-like sound. This practice can help increase relaxation, improve concentration, and promote mindfulness. To practice Ujjayi breathing, sit comfortably with a straight spine and inhale deeply through your nose. As you exhale, constrict the back of your throat to create a hissing sound, as if you're trying to fog up a mirror with your breath. Keep your mouth closed throughout the practice and continue to breathe in and out through your nose. Repeat for several rounds, focusing on the sound and sensation of your breath.",
+  },
+];
+
+export { breathingExercises };
