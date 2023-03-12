b5d3fa4c133972c8407be38fbbd5b3fa1a58d619
Valentin Minkov <minkowalentin@proton.me>
2023-03-12 19:43:28 +0100

    change constants

--- src/constants.js
+++ src/constants.js
@@ -2,8 +2,8 @@ const MINUTE = 60;
 const SECOND = 1000;
 const MIN_WORK_DURATION = 1;
 const MAX_WORK_DURATION = 90;
-const BREAK_LABEL = "Break";
-const WORK_LABEL = "Work";
+const BREAK_LABEL = "Rest";
+const WORK_LABEL = "Focus";
 
 export {
   MINUTE,
