a5c14af18d18e169c5dc4d440279a0d682ccf1f1
Valentin Minkov <minkowalentin@proton.me>
2023-04-12 23:20:39 +0200

    add basic custom notification and a button to trigger it for testing

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,13 +3,23 @@ import styles from "./Layout.module.scss";
 import Button from "../Button/Button";
 import Overlay, { OVERLAY_POSITIONS } from "../Overlay/Overlay";
 import useNotificationPermission from "../../hooks/useNotificationPermission";
+import Notification from "../Notification/Notification";
+import { useNotification } from "../../context/NotificationContext";
 
 const Layout = ({ children }) => {
   const [permission, requestNotificationPermission] =
     useNotificationPermission();
+  const { notifications, addNotification } = useNotification();
+
+  const handleShowNotification = () => {
+    addNotification("This is a custom notification");
+  };
 
   return (
     <>
+      {notifications.map((notification) => (
+        <Notification key={notification.id} message={notification.message} />
+      ))}
       <Overlay
         show={permission === "default"}
         position={OVERLAY_POSITIONS.bottomRight}
@@ -18,6 +28,11 @@ const Layout = ({ children }) => {
           onClick={requestNotificationPermission}
           content={<p>Enable Notifications</p>}
           className={styles["overlay-button"]}
+        />
+        <Button
+          onClick={handleShowNotification}
+          content={<p>Show Custom Notification</p>}
+          className={styles["custom-notification-button"]}
         />
       </Overlay>
       <div className={styles["app-container"]}>{children}</div>
