638c383083dd1f63fe54f3617044d89cf21aa23b
Valentin Minkov <minkowalentin@proton.me>
2023-04-15 22:56:38 +0200

    test: adjust the test for app component

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,41 @@
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
+import { AppContext } from "./context/AppContext";
 
-test('renders app', () => {
-  render(<App />);
+window.confirm = jest.fn(() => true);
+
+jest.mock("./context/AppContext", () => ({
+  AppContext: {
+    Provider: ({ children }) => children,
+  },
+}));
+
+test("renders app", () => {
+  const mockAppState = {
+    isBreakPeriod: false,
+    isRightOn: false,
+  };
+
+  const { container } = render(
+    <AppContext.Provider value={{ appState: mockAppState }}>
+      {AppContext ? <App /> : null}
+    </AppContext.Provider>
+  );
+
+  // Assert that the Pomodoro component is present
+  const pomodoroElement = container.querySelector(".pomodoro-content");
+  expect(pomodoroElement).toBeInTheDocument();
+
+  // Assert that the BreakContent component is present
+  const breakContentElement = container.querySelector(
+    ".break-content-container"
+  );
+  expect(breakContentElement).toBeInTheDocument();
+
+  // Assert that the initial state of isBreakPeriod is false
+  expect(mockAppState.isBreakPeriod).toBe(false);
+
+  // Assert that the initial state of isRightOn is false
+  expect(mockAppState.isRightOn).toBe(false);
 });
